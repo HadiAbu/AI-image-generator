00eca7dc081d26a58573a4653dcdd563a4666a44
Hadi <hadi.abuhamed@gmail.com>
2023-10-24 17:51:40 +0300

    Finished Card on the client and added fileSave function

--- client/src/utils/index.tsx
+++ client/src/utils/index.tsx
@@ -1,4 +1,9 @@
+import FileSaver from "file-saver";
 import { surpriseMePrompts } from "../constants";
+
+export async function downloadImage(_id: string, photo: string) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
 
 export const getRandomPrompt = (prompt: string): string => {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
